096d2089245cc713e3c391f298c967a15c9c552d
ahadhossain <ahadhossain0199@gmail.com>
2026-08-06 01:08:49 +0600

    Update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router'
 import RootLayout from './RootLayout'
 import Banner from './Components/Banner'
+import Project from './Pages/Project'
 
 function App() {
   return (
     <Routes>
       <Route element={<RootLayout />}>
         <Route path="/" element={<Banner />} />
+        <Route path="/projects" element={<Project />} />
         <Route path="*" element={<Banner />} />
       </Route>
     </Routes>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Bannar from '../Components/Bannar'
+import Banner from '../Components/Banner'
 
 const Home = () => {
   return (
     <>
-    <Bannar/>
-      
+      <Banner />
     </>
   )
 }
